4b1a4cdefb877bfe49525e0f53b555da0cd71bbf
ErnestoGarSa <ernest0_10@hotmail.com>
2025-04-16 19:39:36 -0600

    Se actualizan los colores del footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,27 @@
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail, HiLocationMarker } from "react-icons/hi";
+import { HiOutlineMail } from "react-icons/hi";
+
+import classes from "./Footer.module.css";
 
 const Footer = () => {
   let emailAddress = "ernestogarsa.dev@gmail.com";
   return (
-    <footer className="footer ">
-      <h3 className="text-center pt-3 contact">Contact</h3>
-
+    <footer className={`${classes.footer} py-3`}>
+      <h3 className={`text-center  ${classes.footer_contact}`}>Contact</h3>
       <div className="d-flex justify-content-around align-items-center">
         <Link
           href={`mailto:${emailAddress}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <HiOutlineMail className="footer-icon  " />
+          <HiOutlineMail className={`${classes.footer_icon}`} />
         </Link>
         <Link href="https://www.github.com/ErnestoGarSa" target="_blank">
-          <FaGithub className="footer-icon " />
+          <FaGithub className={`${classes.footer_icon}`} />
         </Link>
         <Link href="https://www.linkedin.com/in/ErnestoGarSa" target="_blank">
-          <FaLinkedin className="footer-icon" />
+          <FaLinkedin className={`${classes.footer_icon}`} />
         </Link>
       </div>
     </footer>
