f3291e3960a59be85c838fadb59f58d274959f1f
ErnestoGarSa <ernest0_10@hotmail.com>
2025-04-16 19:03:55 -0600

    Se cambia la pagina principal, la alineación del texto, el texto y colores

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import classes from "./page.module.css";
 
 const page = () => {
   return (
     <>
-      <main className="container  ">
+      <main className={`container ${classes.container_content}`}>
         <div className="d-flex flex-column justify-content-center align-items-center">
-          <h1 className="text-center my-3 title title-main">
+          <h1 className={`text-center mt-5 ${classes.title} `}>
             Mastering the web with JavaScript, React and Next.js
           </h1>
-          <p className="container para  ">
-            Welcome to my personal web project showcasing my proficiency in
-            JavaScript, React, and Next.js. Explore the seamless integration of
-            these cutting-edge technologies as I demonstrate their collective
-            power in creating dynamic, responsive, and user-friendly web
-            applications. Dive into a world of smooth transitions, intuitive
-            user interfaces, and optimized performance, all expertly crafted to
-            reflect my strong command over these powerful frameworks. Experience
-            the synergy of JavaScript, React, and Next.js firsthand, and witness
-            how they come together to shape a seamless, modern web experience
+          <p className="container fs-3 ">
+            Welcome to my personal web project, where I share what I can do with
+            JavaScript, React, and Next.js.
+          </p>
+          <p className="container fs-3">
+            Here, you’ll see how these technologies come together to build
+            modern, dynamic, and user-friendly web apps.
+          </p>
+          <p className="container fs-3">
+            Everything’s designed to feel smooth — with clean transitions,
+            intuitive interfaces, and solid performance.
+          </p>
+          <p className="container fs-3">
+            It’s a simple way to show how I use these tools to create
+            functional, up-to-date websites.
           </p>
         </div>
       </main>
